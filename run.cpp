0c9ebb6055c6d7c1d082c95961fe0adcbecbd9a8
Mohammad Riyaz <116792240+riyazweb@users.noreply.github.com>
2023-05-27 22:01:29 +0530

    Update run.cpp

--- run.cpp
+++ run.cpp
@@ -225,3 +225,39 @@ int main()
      return 0;
 }
 
+switch --------------------------------------------------------------------------------------------------
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+     int day;
+     cin >> day;
+     switch (day){
+     case 1:
+          cout << "mon";
+          break;
+     case 2:
+          cout << "tue";
+           break;
+     case 3:
+          cout << "wed";
+           break;
+     case 4:
+          cout << "thur";
+           break;
+     case 5:
+          cout << "fri";
+           break;
+     case 6:
+          cout << "sat";
+           break;
+     case 7:
+          cout << "sun day hein phenchooo";
+           break;
+     default:
+          cout << "1 to 7 only";
+      
+}
+     return 0;
+}
+array--------------------------------------------------------------------------------------------------------------
