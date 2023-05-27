b9ad71bf95fe18e194d9c0120aaa4f2c2c00ab3c
Mohammad Riyaz <116792240+riyazweb@users.noreply.github.com>
2023-05-27 21:07:01 +0530

    Add files via upload

---
+++ run.cpp
@@ -0,0 +1,82 @@
+
+// basics-----------------------------------------------------------------------------------------------------------
+// #include<iostream>
+// int main() {
+//    std::cout << "hey striver" << "\n";
+//    std::cout << "hey striver" << "/n";
+//    return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//    cout << "hey striver"
+//         << "\n";
+//    cout << "hey striver"
+//         << "\n";
+//    return 0;
+// }
+// #include <iostream>
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//      int x, y;
+//      cout << "enter of x y:" ;
+//      cin >> x >> y;
+//      cout << "value of x + y:" << x + y ;
+//      return 0;
+// }
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//      int x, y;
+//      cout << "enter of x y:" ;
+//      cin >> x >> y;
+//      cout << "value of x + y:" << x + y   << "\n";;
+//      cout << "value of x + y:" << x - y  << "\n";;
+//      cout << "value of x + y:" << x * y  << "\n";;
+//      cout << "value of x + y:" << x / y  << "\n";;
+//      return 0;
+// }
+// data types---------------------------------------------------------------------------------------
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+     // // int
+     // int x = 10;
+     // // long
+     // long x = 15;
+     // cin >> x;
+     // long long
+     // x = 150000000;
+     // // float,
+     
+
+     // float,
+// double
+     // float x = 5.6;
+     // double Y = 5;
+     // cout << x + Y ;;
+ // int , long, long long, float,
+// string and getline
+     // string s1;
+     // string s2;
+     // cin >> s1 >>s2;
+     // s2;
+     // cout << s1 <<" " << s2 ;
+    // string and get line
+     // cout << "enter name:" ;
+     // string str;
+     // string s1 = "your name is: ";
+     // getline(cin, str);
+    
+     // cout  << s1  + str;
+
+     
+
+     return 0;
+}
