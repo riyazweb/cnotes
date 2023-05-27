d8d760cf5888f3adf6f56202b0fdc2ee90393150
Mohammad Riyaz <116792240+riyazweb@users.noreply.github.com>
2023-05-27 21:39:54 +0530

    Update run.cpp

--- run.cpp
+++ run.cpp
@@ -46,37 +46,108 @@ data types----------------------------------------------------------------------
 using namespace std;
 int main()
 {
-     // int
-     int x = 10;
-     // long
-     long x = 15;
-     cin >> x;
-     long long
-     x = 150000000;
-     // float,
-     
+// int
+int x = 10;
+// long
+long x = 15;
+cin >> x;
+long long
+x = 150000000;
+// float,
 
-     float,
+float,
 double
-     float x = 5.6;
-     double Y = 5;
-     cout << x + Y ;;
- int , long, long long, float,
+float x = 5.6;
+double Y = 5;
+cout << x + Y ;;
+int , long, long long, float,
 string and getline
-     string s1;
-     string s2;
-     cin >> s1 >>s2;
-     s2;
-     cout << s1 <<" " << s2 ;
-    string and get line
-     cout << "enter name:" ;
-     string str;
-     string s1 = "your name is: ";
-     getline(cin, str);
-    
-     cout  << s1  + str;
+string s1;
+string s2;
+cin >> s1 >>s2;
+s2;
+cout << s1 <<" " << s2 ;
+string and get line
+cout << "enter name:" ;
+string str;
+string s1 = "your name is: ";
+getline(cin, str);
 
-     
+cout  << s1  + str;
+
+char Ch = 'h';
+cout << Ch;
 
      return 0;
+}
+if else ----------------------------------------------------------------------------------------------
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int age;
+    cin >> age;
+     if(age>=18){
+      cout << "you are ready for everything";
+     }
+     else
+     {
+      cout << "no you are not allowed";
+     }
+     return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int age;
+    cin >> age;
+     if(age>=18){
+      cout << "you are ready for everything";
+     }
+     else if (age<=18)
+     {
+      cout << "no you are not allowed";
+     }
+     return 0;
+}
+ #include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int marks;
+    cin >> marks;
+     if(marks<=10){
+      cout << "LOWDE";
+     }
+     else if (marks<=20)
+     {
+      cout << "F";
+     }
+       else if (marks<=30)
+     {
+      cout << "E";
+     }
+       else if (marks<=40)
+     {
+      cout << "D";
+     }
+       else if (marks<=50)
+     {
+      cout << "B";
+     }
+       else if (marks<=60)
+     {
+      cout << "B";
+     }
+       else if (marks<=70)
+     {
+      cout << "A";
+     }
+     return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
 }
