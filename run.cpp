503bb9169c18315742bda7491942dd1281ae6e38
Mohammad Riyaz <116792240+riyazweb@users.noreply.github.com>
2023-05-28 00:11:06 +0530

    Update run.cpp

--- run.cpp
+++ run.cpp
@@ -256,8 +256,80 @@ int main()
            break;
      default:
           cout << "1 to 7 only";
-      
+
 }
      return 0;
 }
 array--------------------------------------------------------------------------------------------------------------
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+#ifndef ONLINE_JUDGE
+
+     // For getting input from input.txt file
+     freopen("input.txt", "r", stdin);
+
+     // Printing the Output to output.txt file
+     freopen("output.txt", "w", stdout);
+#endif
+     int arr[4];
+     cin >> arr[0] >> arr[1] >> arr[2] >> arr[3] >> arr[4];
+     arr[4]+=100;
+     cout << arr[4];
+
+
+
+
+
+
+     return 0;
+} 
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+#ifndef ONLINE_JUDGE
+
+     // For getting input from input.txt file
+     freopen("input.txt", "r", stdin);
+
+     // Printing the Output to output.txt file
+     freopen("output.txt", "w", stdout);
+#endif
+     int arr[4][4];
+     arr[4][4] = 5;
+     cout << arr[4][4];
+
+
+
+
+
+
+     return 0;
+} 
+
+extras-----------------------------------------------------------------------------------------------------
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+#ifndef ONLINE_JUDGE
+
+     // For getting input from input.txt file
+     freopen("input.txt", "r", stdin);
+
+     // Printing the Output to output.txt file
+     freopen("output.txt", "w", stdout);
+#endif
+     string r ="riyaz";
+     // cout << r[2];
+     int len = r.size();
+     r[len-1] = 'c';
+     cout << r[len-1];
+
+
+
+     return 0;
+} 
+for loops------------------------------------------------------------------------------
