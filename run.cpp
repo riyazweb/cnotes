4cb1c08d946b1161eef9c10743e3029bf43e0c61
Mohammad Riyaz <116792240+riyazweb@users.noreply.github.com>
2023-05-28 18:36:05 +0530

    Update run.cpp

--- run.cpp
+++ run.cpp
@@ -2,8 +2,8 @@
 basics-----------------------------------------------------------------------------------------------------------
 #include<iostream>
 int main() {
-   std::cout << "hey striver" << "\n";
-   std::cout << "hey striver" << "/n";
+   std::cout << "hey rahul good boy" << "\n"; //wrong
+   std::cout << "hey rahul erri hook" << "/n";
    return 0;
 }
 
@@ -278,13 +278,8 @@ int main()
      arr[4]+=100;
      cout << arr[4];
 
-
-
-
-
-
      return 0;
-} 
+}
 #include <bits/stdc++.h>
 using namespace std;
 int main()
@@ -301,13 +296,8 @@ int main()
      arr[4][4] = 5;
      cout << arr[4][4];
 
-
-
-
-
-
      return 0;
-} 
+}
 
 extras-----------------------------------------------------------------------------------------------------
 #include <bits/stdc++.h>
@@ -328,8 +318,281 @@ int main()
      r[len-1] = 'c';
      cout << r[len-1];
 
+     return 0;
+}
+for loops------------------------------------------------------------------------------
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+#ifndef ONLINE_JUDGE
 
+     // For getting input from input.txt file
+     freopen("input.txt", "r", stdin);
+
+     // Printing the Output to output.txt file
+     freopen("output.txt", "w", stdout);
+#endif
+    int i;
+    for(i=1;i<=5; i=i+1){
+        cout<< "riyaz" << "\n" ;
+        // we can write nest here too
+    }
+     return 0;
+}
+while loops-----------------------------------------------------------------------------------------------------------
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+#ifndef ONLINE_JUDGE
+
+     // For getting input from input.txt file
+     freopen("input.txt", "r", stdin);
+
+     // Printing the Output to output.txt file
+     freopen("output.txt", "w", stdout);
+#endif
+    int i=1;
+    while(i<=5){
+        cout << "riyaz" << "\n" ;
+        i = i + 1;
+
+    }
 
      return 0;
-} 
-for loops------------------------------------------------------------------------------
+}
+
+do--------------------------------------------------------------------------------------------------------------------------------
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+#ifndef ONLINE_JUDGE
+
+     // For getting input from input.txt file
+     freopen("input.txt", "r", stdin);
+
+     // Printing the Output to output.txt file
+     freopen("output.txt", "w", stdout);
+#endif
+    int i=2;
+    do{
+        cout <<"Stiver";
+        i=i+1;
+        }while(i<=1);
+cout << i;
+return 0;
+}
+fucntions----------------------------------------------------------------------------------------------------------------------------------------
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void riyaz(string name){
+  cout << "you name is " << name ;
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+
+     // For getting input from input.txt file
+     freopen("input.txt", "r", stdin);
+
+     // Printing the Output to output.txt file
+     freopen("output.txt", "w", stdout);
+#endif
+    string name;
+    cout << "enter your name;";
+    cin >> name;
+    riyaz(name);
+
+return 0;
+}
+ #include <bits/stdc++.h>
+using namespace std;
+
+int riyaz(int a , int b ){
+  cout << "addition is " ;
+  int res =  a+b;
+  return res;
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+
+     // For getting input from input.txt file
+     freopen("input.txt", "r", stdin);
+
+     // Printing the Output to output.txt file
+     freopen("output.txt", "w", stdout);
+#endif
+int a,b;
+cin>> a >> b;
+int res = riyaz(a,b);
+cout<< res;
+// int a,b;
+// cin >> a >> b;
+// cout<< a + b;
+
+return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+
+int riyaz(int a , int b ){
+  if(a>=b){
+    return a;
+  }
+  else{ return b;}
+
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+
+     // For getting input from input.txt file
+     freopen("input.txt", "r", stdin);
+
+     // Printing the Output to output.txt file
+     freopen("output.txt", "w", stdout);
+#endif
+int a,b;
+cin>> a >> b;
+int res = riyaz(a,b);
+cout<< res;
+// int a,b;
+// cin >> a >> b;
+// cout<< a + b;
+
+return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+
+int riyaz(int a ){
+ cout << a+1;
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+
+     // For getting input from input.txt file
+     freopen("input.txt", "r", stdin);
+
+     // Printing the Output to output.txt file
+     freopen("output.txt", "w", stdout);
+#endif
+int a;
+cin>> a ;
+int res = riyaz(a);
+//pass by value
+ cout << a;
+// int a,b;
+// cin >> a >> b;
+// cout<< a + b;
+
+return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+
+// pass by reference &a
+void riyaz(string &a ){
+  a[0]='f';
+cout<< a;
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+
+     // For getting input from input.txt file
+     freopen("input.txt", "r", stdin);
+
+     // Printing the Output to output.txt file
+     freopen("output.txt", "w", stdout);
+#endif
+string  a ="duckrahul";
+
+riyaz(a);
+//pass by value
+
+// int a,b;
+// cin >> a >> b;
+// cout<< a + b;
+
+return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+
+// pass by reference &a
+void riyaz(string &a ){
+  a[0]='f';
+cout<< a;
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+
+     // For getting input from input.txt file
+     freopen("input.txt", "r", stdin);
+
+     // Printing the Output to output.txt file
+     freopen("output.txt", "w", stdout);
+#endif
+
+int arr[5];//ont forget array ielese than-1 than number
+for(int i = 0; i<=4;i=i+1){
+    cin>> arr[i];
+}
+//if not cin no cout
+for(int i = 0; i<=4;i=i+1){
+    cout<< arr[i] << " ";
+}
+
+return 0;
+}
+
+if not cin no cout
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+void riyaz(int arr[], int n)
+{
+    arr[0] += 1000;
+    cout << "value is " << arr[0];
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+
+    // For getting input from input.txt file
+    freopen("input.txt", "r", stdin);
+
+    // Printing the Output to output.txt file
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int n = 5;
+    int arr[n]; // ont forget array ielese than-1 than number
+    for (int i = 0; i < n; i = i + 1)
+    {
+        cin >> arr[i];
+    }
+    riyaz(arr, n);
+    cout << "\n"
+         << "value is " << arr[0];
+    return 0;
+}
+// result: value is 1006 value is 1006 because array always go with refrence means it doesnt stop at riyaz(arr,n);
